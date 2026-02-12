8a74e0ee16483f6fcb50d0eeec49508fa15ea83c
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-02-12 14:26:25 +0300

    hot fix header nav

--- auth-service-frontend/src/Header.tsx
+++ auth-service-frontend/src/Header.tsx
@@ -1,46 +1,44 @@
-import { useLocation } from "react-router-dom";
-import { Layout, Menu, type MenuProps } from "antd";
+import { Link, useLocation } from "react-router-dom";
+import { Layout, Menu } from "antd";
 import { useAuth } from "./context/AuthContext";
 import { AUTH_SERVICE_PREFIX } from "./const";
 const { Header } = Layout;
-
-
 
 export function HeaderComponent() {
   const location = useLocation()
   const { logout, user } = useAuth();
 
-  const items: MenuProps["items"] = [
-    {
-      key: `${AUTH_SERVICE_PREFIX}/users`,
-      label: 'Пользователи'
-    },
-    {
-      key: `${AUTH_SERVICE_PREFIX}/roles`,
-      label: 'Роли'
-    },
-    {
-      key: `${AUTH_SERVICE_PREFIX}/privileges`,
-      label: 'Права'
-    },
-    {
-      key: `${AUTH_SERVICE_PREFIX}/sessions`,
-      label: 'Сессии'
-    },
-    {
-      key: `${AUTH_SERVICE_PREFIX}/email-configs`,
-      label: 'Конфиг email'
-    },
-    {
-      onClick: logout,
-      key: "key",
-      label: "Выйти"
-    }
-  ]
-
   return (
       <Header>
-        {user && <Menu items={items} theme="dark" mode="horizontal" selectedKeys={[location.pathname]}></Menu>}
+        <Menu theme="dark" mode="horizontal" selectedKeys={[location.pathname]}>
+          {user && (
+            <>
+                <Menu.Item key={`${AUTH_SERVICE_PREFIX}/users`}>
+                  <Link to={`${AUTH_SERVICE_PREFIX}/users`}>Пользователи</Link>
+                </Menu.Item>
+
+                <Menu.Item key={`${AUTH_SERVICE_PREFIX}/roles`}>
+                  <Link to={`${AUTH_SERVICE_PREFIX}/roles`}>Роли</Link>
+                </Menu.Item>
+
+                <Menu.Item key={`${AUTH_SERVICE_PREFIX}/privileges`}>
+                  <Link to={`${AUTH_SERVICE_PREFIX}/privileges`}>Права</Link>
+                </Menu.Item>
+
+                <Menu.Item key={`${AUTH_SERVICE_PREFIX}/sessions`}>
+                  <Link to={`${AUTH_SERVICE_PREFIX}/sessions`}>Сессии</Link>
+                </Menu.Item>
+
+                <Menu.Item key={`${AUTH_SERVICE_PREFIX}/email-configs`}>
+                  <Link to={`${AUTH_SERVICE_PREFIX}/email-configs`}>Конфиг email</Link>
+                </Menu.Item>
+
+                <Menu.Item key="logout" onClick={logout}>
+                  Выйти
+                </Menu.Item>
+            </>
+          )}
+        </Menu>
       </Header>
   );
 }
